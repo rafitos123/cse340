8fd41fac3f2e46799e12cc0920f4ff324e064a8c
lolrafael123 <rafael.antunes2705@gmail.com>
2025-09-06 20:53:47 -0300

    changes

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ const static = require("./routes/static")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "layouts/layout")
+app.use(express.static('public'));
+
 
 /* ***********************
  * Routes
